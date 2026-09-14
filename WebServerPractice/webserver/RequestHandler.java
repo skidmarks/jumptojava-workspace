5dd2df5f087b8894edf999824615148a8439679e
skidmarks <florakid@naver.com>
2026-09-14 16:43:01 +0900

    텍스트 내용으로 변경함

--- WebServerPractice/webserver/RequestHandler.java
+++ WebServerPractice/webserver/RequestHandler.java
@@ -1,56 +1,59 @@
 package webserver;
 
-import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
-public class RequestHandler implements Runnable {
+public class RequestHandler extends Thread {
+
+    private static final Logger log = System.getLogger(RequestHandler.class.getName());
+
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
-    @Override
     public void run() {
-        System.out.println("New Client Connect! Connected IP : " + connection.getInetAddress()
-                + ", Port : " + connection.getPort());
+        log.log(Level.DEBUG, "New Client connnect! connected IP : {0}, Port : {1}", connection.getInetAddress(),
+                connection.getPort());
 
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-             OutputStream out = connection.getOutputStream()) {
-
-            String line = br.readLine();
-            if (line == null) {
-                return;
-            }
-            System.out.println("request line : " + line);
-
-            while (!(line = br.readLine()).isEmpty()) {
-                System.out.println("header : " + line);
-            }
-
-            byte[] body = "<h1>Hello florakid!너</h1>".getBytes(StandardCharsets.UTF_8);
-            response200Header(out, body.length);
-            responseBody(out, body);
+        try (InputStream in = connection.getInputStream();
+                OutputStream out = connection.getOutputStream()) {
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.log(Level.ERROR, e.getMessage());
         }
     }
 
-    private void response200Header(OutputStream out, int lengthOfBodyContent) throws IOException {
-        out.write("HTTP/1.1 200 OK \r\n".getBytes());
-        out.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
-        out.write(("Content-Length: " + lengthOfBodyContent + "\r\n").getBytes());
-        out.write("\r\n".getBytes());
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+
+        } catch (IOException e) {
+            log.log(Level.ERROR, e.getMessage());
+        }
     }
 
-    private void responseBody(OutputStream out, byte[] body) throws IOException {
-        out.write(body);
-        out.flush();
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.ERROR, e.getMessage());
+        }
     }
 }
